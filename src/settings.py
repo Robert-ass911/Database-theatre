c2f693e0eab1cf0ae72729808bae2556152355ee
Robert-ass911 <112751073+Robert-ass911@users.noreply.github.com>
2023-12-22 03:52:05 +0300

    Add files via upload

---
+++ src/settings.py
@@ -0,0 +1,4 @@
+DB_PATH = 'C:/Users/samar/PycharmProjects/teator/database/teator.db'
+SCRIPTS_TABLES_PATH = 'C:/Users/samar/PycharmProjects/teator/database/scripts/base.sql'
+HOST = '127.0.0.1'
+PORT = 8000
