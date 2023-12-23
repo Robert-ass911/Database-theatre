6693669aafa0f19bb7dc76786d8c4924f9643f6e
Robert-ass911 <112751073+Robert-ass911@users.noreply.github.com>
2023-12-23 12:51:43 +0300

    Delete src directory

--- src/settings.py
+++
@@ -1,4 +0,0 @@
-DB_PATH = 'C:/Users/samar/PycharmProjects/teator/database/teator.db'
-SCRIPTS_TABLES_PATH = 'C:/Users/samar/PycharmProjects/teator/database/scripts/base.sql'
-HOST = '127.0.0.1'
-PORT = 8000
